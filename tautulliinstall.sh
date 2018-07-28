871606572de711b458b78a4a0fb7e888d63a454b
NasKar2 <jschwrtz9@gmail.com>
2018-07-28 14:17:26 -0400

    tautulli install script

---
+++ tautulliinstall.sh
@@ -0,0 +1,151 @@
+#!/bin/sh
+# Build an iocage jail under FreeNAS 11.1 with  tautulli
+# https://github.com/NasKar2/sepapps-freenas-iocage
+
+# Check for root privileges
+if ! [ $(id -u) = 0 ]; then
+   echo "This script must be run with root privileges"
+   exit 1
+fi
+
+# Initialize defaults
+JAIL_IP=""
+DEFAULT_GW_IP=""
+INTERFACE=""
+VNET="off"
+POOL_PATH=""
+APPS_PATH=""
+TAUTULLI_DATA=""
+MEDIA_LOCATION=""
+TORRENTS_LOCATION=""
+
+
+SCRIPT=$(readlink -f "$0")
+SCRIPTPATH=$(dirname "$SCRIPT")
+. $SCRIPTPATH/tautulli-config
+CONFIGS_PATH=$SCRIPTPATH/configs
+
+# Check for tautulli-config and set configuration
+if ! [ -e $SCRIPTPATH/tautulli-config ]; then
+  echo "$SCRIPTPATH/tautulli-config must exist."
+  exit 1
+fi
+
+# Check that necessary variables were set by tautulli-config
+if [ -z $JAIL_IP ]; then
+  echo 'Configuration error: JAIL_IP must be set'
+  exit 1
+fi
+if [ -z $DEFAULT_GW_IP ]; then
+  echo 'Configuration error: DEFAULT_GW_IP must be set'
+  exit 1
+fi
+if [ -z $INTERFACE ]; then
+  echo 'Configuration error: INTERFACE must be set'
+  exit 1
+fi
+if [ -z $POOL_PATH ]; then
+  echo 'Configuration error: POOL_PATH must be set'
+  exit 1
+fi
+
+if [ -z $APPS_PATH ]; then
+  echo 'Configuration error: APPS_PATH must be set'
+  exit 1
+fi
+
+if [ -z $JAIL_NAME ]; then
+  echo 'Configuration error: JAIL_NAME must be set'
+  exit 1
+fi
+
+if [ -z $TAUTULLI_DATA ]; then
+  echo 'Configuration error: TAUTULLI_DATA must be set'
+  exit 1
+fi
+
+if [ -z $MEDIA_LOCATION ]; then
+  echo 'Configuration error: MEDIA_LOCATION must be set'
+  exit 1
+fi
+
+if [ -z $TORRENTS_LOCATION ]; then
+  echo 'Configuration error: TORRENTS_LOCATION must be set'
+  exit 1
+fi
+
+#
+# Create Jail
+echo '{"pkgs":["nano","python2","py27-sqlite3","py27-openssl","ca_root_nss","git"]}' > /tmp/pkg.json
+iocage create --name "${JAIL_NAME}" -p /tmp/pkg.json -r 11.1-RELEASE ip4_addr="${INTERFACE}|${JAIL_IP}/24" defaultrouter="${DEFAULT_GW_IP}" boot="on" host_hostname="${JAIL_NAME}" vnet="${VNET}"
+
+rm /tmp/pkg.json
+
+#
+# needed for installing from ports
+#mkdir -p ${PORTS_PATH}/ports
+#mkdir -p ${PORTS_PATH}/db
+
+mkdir -p ${POOL_PATH}/${APPS_PATH}/${TAUTULLI_DATA}
+#mkdir -p ${POOL_PATH}/${APPS_PATH}/${RADARR_DATA}
+#mkdir -p ${POOL_PATH}/${APPS_PATH}/${LIDARR_DATA}
+#mkdir -p ${POOL_PATH}/${APPS_PATH}/${SABNZBD_DATA}
+#mkdir -p ${POOL_PATH}/${APPS_PATH}/${PLEX_DATA}
+mkdir -p ${POOL_PATH}/${MEDIA_LOCATION}
+mkdir -p ${POOL_PATH}/${TORRENTS_LOCATION}
+echo "mkdir -p '${POOL_PATH}/${APPS_PATH}/${TAUTULLI_DATA}'"
+#echo "mkdir -p '${POOL_PATH}/${APPS_PATH}/${SABNZBD_DATA}'"
+
+tautulli_config=${POOL_PATH}/${APPS_PATH}/${TAUTULLI_DATA}
+#iocage exec ${JAIL_NAME} mkdir -p /mnt/configs
+iocage exec ${JAIL_NAME} 'sysrc ifconfig_epair0_name="epair0b"'
+
+#
+# mount ports so they can be accessed in the jail
+#iocage fstab -a ${JAIL_NAME} ${PORTS_PATH}/ports /usr/ports nullfs rw 0 0
+#iocage fstab -a ${JAIL_NAME} ${PORTS_PATH}/db /var/db/portsnap nullfs rw 0 0
+
+iocage fstab -a ${JAIL_NAME} ${CONFIGS_PATH} /mnt/configs nullfs rw 0 0
+iocage fstab -a ${JAIL_NAME} ${tautulli_config} /config nullfs rw 0 0
+iocage fstab -a ${JAIL_NAME} ${POOL_PATH}/${MEDIA_LOCATION} /mnt/media nullfs rw 0 0
+iocage fstab -a ${JAIL_NAME} ${POOL_PATH}/${TORRENTS_LOCATION} /mnt/torrents nullfs rw 0 0
+
+iocage restart ${JAIL_NAME}
+  
+# add media group to media user
+#iocage exec ${JAIL_NAME} pw groupadd -n media -g 8675309
+#iocage exec ${JAIL_NAME} pw groupmod media -m media
+#iocage restart ${JAIL_NAME} 
+
+
+#
+# Install tautulli
+iocage fstab -a ${JAIL_NAME} ${tautulli_config} /config nullfs rw 0 0
+iocage exec ${JAIL_NAME} git clone https://github.com/Tautulli/Tautulli.git /usr/local/share/Tautulli
+iocage exec ${JAIL_NAME} "pw user add tautulli -c tautulli -u 109 -d /nonexistent -s /usr/bin/nologin"
+iocage exec ${JAIL_NAME} chown -R tautulli:tautulli /usr/local/share/Tautulli /config
+iocage exec ${JAIL_NAME} cp /usr/local/share/Tautulli/init-scripts/init.freenas /usr/local/etc/rc.d/tautulli
+iocage exec ${JAIL_NAME} chmod u+x /usr/local/etc/rc.d/tautulli
+iocage exec ${JAIL_NAME} sysrc "tautulli_enable=YES"
+iocage exec ${JAIL_NAME} sysrc "tautulli_flags=--datadir /config"
+iocage exec ${JAIL_NAME} service tautulli start
+echo "Tautulli installed"
+
+#
+# Make pkg upgrade get the latest repo
+iocage exec ${JAIL_NAME} mkdir -p /usr/local/etc/pkg/repos/
+iocage exec ${JAIL_NAME} cp -f /mnt/configs/FreeBSD.conf /usr/local/etc/pkg/repos/FreeBSD.conf
+
+#
+# Upgrade to the lastest repo
+iocage exec ${JAIL_NAME} pkg upgrade -y
+iocage restart ${JAIL_NAME}
+
+#
+# remove /mnt/configs as no longer needed
+#iocage fstab -r ${JAIL_NAME} ${CONFIGS_PATH} /mnt/configs nullfs rw 0 0
+
+echo
+
+echo "Tautulli should be available at http://${JAIL_IP}:8989"
+
