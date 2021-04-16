60e97ee0d1f093b841330c42ad292be2d8297983
root <root@freenasSuper.local>
2021-04-16 15:03:19 -0400

    remove sonarr-devel for sonarr

--- sonarrinstall3.sh
+++ sonarrinstall3.sh
@@ -78,7 +78,7 @@ fi
 
 #
 # Create Jail
-echo '{"pkgs":["nano","sonarr-devel","libiconv"]}' > /tmp/pkg.json
+echo '{"pkgs":["nano","sonarr","libiconv"]}' > /tmp/pkg.json
 if ! iocage create --name "${JAIL_NAME}" -p /tmp/pkg.json -r "${RELEASE}" ip4_addr="${INTERFACE}|${JAIL_IP}/24" defaultrouter="${DEFAULT_GW_IP}" boot="on" host_hostname="${JAIL_NAME}" vnet="${VNET}"
 then
 	echo "Failed to create jail"
