ca019420bc45392b1e541de671d390ec241bd44c
root <root@freenasSuper.local>
2021-06-09 22:18:53 -0400

    add /temp to lidarr

--- lidarrinstall.sh
+++ lidarrinstall.sh
@@ -107,6 +107,7 @@ fi
 mkdir -p ${POOL_PATH}/${APPS_PATH}/${LIDARR_DATA}
 #mkdir -p ${POOL_PATH}/${APPS_PATH}/${SABNZBD_DATA}
 #mkdir -p ${POOL_PATH}/${APPS_PATH}/${PLEX_DATA}
+mkdir -p /temp/downloads/sabnzbd/complete
 mkdir -p ${POOL_PATH}/${MEDIA_LOCATION}/videos/music
 mkdir -p ${POOL_PATH}/${MEDIA_LOCATION}/${SABNZBD_DOWNLOADS}
 mkdir -p ${POOL_PATH}/${TORRENTS_LOCATION}
@@ -134,7 +135,7 @@ iocage exec ${JAIL_NAME} mkdir -p /mnt/media/${SABNZBD_DOWNLOADS}
 # mount ports so they can be accessed in the jail
 #iocage fstab -a ${JAIL_NAME} ${PORTS_PATH}/ports /usr/ports nullfs rw 0 0
 #iocage fstab -a ${JAIL_NAME} ${PORTS_PATH}/db /var/db/portsnap nullfs rw 0 0
-
+iocage fstab -a ${JAIL_NAME} /temp /temp nullfs rw 0 0
 iocage fstab -a ${JAIL_NAME} ${CONFIGS_PATH} /mnt/configs nullfs rw 0 0
 iocage fstab -a ${JAIL_NAME} ${lidarr_config} /config nullfs rw 0 0
 iocage fstab -a ${JAIL_NAME} ${POOL_PATH}/${MEDIA_LOCATION} /mnt/media nullfs rw 0 0
