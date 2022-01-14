58497b01e3f4a17d0f597843009b9b4f15f88482
root <root@freenasSuper.local>
2022-01-14 17:02:58 -0500

    update tautulli to py38

--- tautulliinstall.sh
+++ tautulliinstall.sh
@@ -68,7 +68,7 @@ fi
 
 #
 # Create Jail
-echo '{"pkgs":["nano","python","py37-sqlite3","py37-openssl","ca_root_nss","git"]}' > /tmp/pkg.json
+echo '{"pkgs":["nano","python","py38-setuptools","py38-sqlite3","py38-openssl","py38-pycryptodomex","ca_root_nss","git-lite"]}' > /tmp/pkg.json
 if ! iocage create --name "${JAIL_NAME}" -p /tmp/pkg.json -r "${RELEASE}" ip4_addr="${INTERFACE}|${JAIL_IP}/24" defaultrouter="${DEFAULT_GW_IP}" boot="on" host_hostname="${JAIL_NAME}" vnet="${VNET}" ${USE_BASEJAIL}
 then
 	echo "Failed to create jail"
