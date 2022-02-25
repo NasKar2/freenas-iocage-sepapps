3b8d977b47dbbcd65020ac527096bb34be39ed2b
NasKar2 <jschwrtz9@gmail.com>
2022-02-24 20:20:00 -0500

    lidarr simplify

--- lidarrinstall.sh
+++ lidarrinstall.sh
@@ -50,7 +50,6 @@ if [ -z $VNET ]; then
   VNET="on"
   echo "VNET defaulting to 'on'"
 fi
-
 if [ -z $POOL_PATH ]; then
   POOL_PATH="/mnt/$(iocage get -p)"
   echo "POOL_PATH defaulting to "$POOL_PATH
@@ -59,7 +58,6 @@ if [ -z $APPS_PATH ]; then
   APPS_PATH="apps"
   echo "APPS_PATH defaulting to 'apps'"
 fi
-
 if [ -z $JAIL_NAME ]; then
   JAIL_NAME="lidarr"
   echo "JAIL_NAME defaulting to 'lidarr'"
@@ -82,43 +80,35 @@ fi
 
 #
 # Create Jail
-#echo '{"pkgs":["nano","mono","mediainfo","sqlite3","ca_root_nss","curl","chromaprint"]}' > /tmp/pkg.json
-#echo '{"pkgs":["nano","mediainfo","sqlite3","libchromaprint-tools","curl"]}' > /tmp/pkg.json
-echo '{"pkgs":["libunwind","icu","libinotify","openssl","mediainfo","sqlite3","ca_root_nss","libiconv","nano","curl","wget"]}' > /tmp/pkg.json
-if ! iocage create --name "${JAIL_NAME}" -p /tmp/pkg.json -r "${RELEASE}" ip4_addr="${INTERFACE}|${JAIL_IP}/24" defaultrouter="${DEFAULT_GW_IP}" boot="on" host_hostname="${JAIL_NAME}" vnet="${VNET}" ${USE_BASEJAIL}
+echo '{"pkgs":["nano"]}' > /tmp/pkg.json
+if ! iocage create --name "${JAIL_NAME}" -p /tmp/pkg.json -r "${RELEASE}" ip4_addr="${INTERFACE}|${JAIL_IP}/24" defaultrouter="${DEFAULT_GW_IP}" boot="on" allow_mlock="1" allow_raw_sockets="1" host_hostname="${JAIL_NAME}" vnet="${VNET}" ${USE_BASEJAIL} allow_mlock=1 allow_raw_sockets=1
 then
 	echo "Failed to create jail"
 	exit 1
 fi
 rm /tmp/pkg.json
-
-# fix 'libdl.so.1 missing' error in 11.1 versions, by reinstalling packages from older FreeBSD release
-# source: https://forums.freenas.org/index.php?threads/openvpn-fails-in-jail-with-libdl-so-1-not-found-error.70391/
-if [ "${RELEASE}" = "11.1-RELEASE" ]; then
-  iocage exec ${JAIL_NAME} sed -i '' "s/quarterly/release_2/" /etc/pkg/FreeBSD.conf
+#
+# Update pkg
+  iocage exec ${JAIL_NAME} "mkdir -p /usr/local/etc/pkg/repos/"
+  iocage exec ${JAIL_NAME} cp /etc/pkg/FreeBSD.conf /usr/local/etc/pkg/repos/FreeBSD.conf
+  iocage exec ${JAIL_NAME} sed -i '' "s/quarterly/latest/" /usr/local/etc/pkg/repos/FreeBSD.conf
   iocage exec ${JAIL_NAME} pkg update -f
   iocage exec ${JAIL_NAME} pkg upgrade -yf
-fi
+
+iocage exec ${JAIL_NAME} pkg install -y lidarr
 #
 # needed for installing from ports
 #mkdir -p ${PORTS_PATH}/ports
 #mkdir -p ${PORTS_PATH}/db
-
 mkdir -p ${POOL_PATH}/${APPS_PATH}/${LIDARR_DATA}
-mkdir -p /temp/downloads/sabnzbd/complete
-mkdir -p ${POOL_PATH}/${MEDIA_LOCATION}/videos/music
-mkdir -p ${POOL_PATH}/${MEDIA_LOCATION}/${SABNZBD_DOWNLOADS}
+mkdir -p ${POOL_PATH}/${MEDIA_LOCATION}/music
 mkdir -p ${POOL_PATH}/${TORRENTS_LOCATION}
-echo "mkdir -p '${POOL_PATH}/${APPS_PATH}/${LIDARR_DATA}'"
-#echo "mkdir -p '${POOL_PATH}/${APPS_PATH}/${SABNZBD_DATA}'"
+mkdir -p ${POOL_PATH}/temp/downloads/sabnzbd/complete/music/
 
-#sonarr_config=${POOL_PATH}/${APPS_PATH}/${SONARR_DATA}
-#radarr_config=${POOL_PATH}/${APPS_PATH}/${RADARR_DATA}
+echo "mkdir -p '${POOL_PATH}/${APPS_PATH}/${LIDARR_DATA}'"
+chown -R media:media ${POOL_PATH}/${MEDIA_LOCATION}
+
 lidarr_config=${POOL_PATH}/${APPS_PATH}/${LIDARR_DATA}
-#sabnzbd_config=${POOL_PATH}/${APPS_PATH}/${SABNZBD_DATA}
-#plex_config=${POOL_PATH}/${APPS_PATH}/${PLEX_DATA}
-#iocage exec ${JAIL_NAME} mkdir -p /mnt/configs
-iocage exec ${JAIL_NAME} 'sysrc ifconfig_epair0_name="epair0b"'
 
 # create dir in jail for mount points
 iocage exec ${JAIL_NAME} mkdir -p /usr/ports
@@ -127,7 +117,7 @@ iocage exec ${JAIL_NAME} mkdir -p /config
 iocage exec ${JAIL_NAME} mkdir -p /mnt/media
 iocage exec ${JAIL_NAME} mkdir -p /mnt/configs
 iocage exec ${JAIL_NAME} mkdir -p /mnt/torrents
-iocage exec ${JAIL_NAME} mkdir -p /mnt/media/${SABNZBD_DOWNLOADS}
+iocage exec ${JAIL_NAME} "mkdir -p /temp/downloads/sabnzbd/complete/music"
 
 #
 # mount ports so they can be accessed in the jail
@@ -138,51 +128,32 @@ iocage fstab -a ${JAIL_NAME} ${CONFIGS_PATH} /mnt/configs nullfs rw 0 0
 iocage fstab -a ${JAIL_NAME} ${lidarr_config} /config nullfs rw 0 0
 iocage fstab -a ${JAIL_NAME} ${POOL_PATH}/${MEDIA_LOCATION} /mnt/media nullfs rw 0 0
 iocage fstab -a ${JAIL_NAME} ${POOL_PATH}/${TORRENTS_LOCATION} /mnt/torrents nullfs rw 0 0
-
-iocage restart ${JAIL_NAME}
-  
-# add media group to media user
-#iocage exec ${JAIL_NAME} pw groupadd -n media -g 8675309
-#iocage exec ${JAIL_NAME} pw groupmod media -m media
-#iocage restart ${JAIL_NAME} 
+iocage fstab -a ${JAIL_NAME} ${POOL_PATH}/temp/downloads/sabnzbd/complete/music /temp/downloads/sabnzbd/complete/music nullfs rw 0 0
 
 #
 # Install Lidarr
-iocage exec ${JAIL_NAME} "fetch https://github.com/lidarr/Lidarr/releases/download/v0.8.1.2135/Lidarr.master.0.8.1.2135.linux-core-x64.tar.gz -o /usr/local/share"
-iocage exec ${JAIL_NAME} "tar -xzvf /usr/local/share/Lidarr.master.*.linux-core-x64.tar.gz -C /usr/local/share"
-iocage exec ${JAIL_NAME} "rm /usr/local/share/Lidarr.master.*.linux-core-x64.tar.gz"
-#iocage exec ${JAIL_NAME} "fetch https://github.com/lidarr/Lidarr/releases/download/v0.8.0.2042/Lidarr.develop.0.8.0.2042.linux.tar.gz -o /usr/local/share"
-#iocage exec ${JAIL_NAME} "tar -xzvf /usr/local/share/Lidarr.develop.*.linux.tar.gz -C /usr/local/share"
-#iocage exec ${JAIL_NAME} "rm /usr/local/share/Lidarr.develop.*.linux.tar.gz"
+#iocage exec ${JAIL_NAME} mkdir -p /mnt/torrents/sabnzbd/incomplete
+#iocage exec ${JAIL_NAME} mkdir -p /mnt/torrents/sabnzbd/complete
+
+#
+# Add user media to jail
 iocage exec ${JAIL_NAME} "pw user add media -c media -u 8675309  -d /nonexistent -s /usr/bin/nologin"
-iocage exec ${JAIL_NAME} chown -R media:media /usr/local/share/Lidarr /config
-#iocage exec ${JAIL_NAME} cp -f /mnt/configs/lidarr /usr/local/etc/rc.d/lidarr
-iocage exec ${JAIL_NAME} chmod u+x /usr/local/etc/rc.d/lidarr
-#iocage exec ${JAIL_NAME} chown -R media:media /usr/local/etc/rc.d/lidarr
-iocage exec ${JAIL_NAME} sed -i '' "s/lidarrdata/${LIDARR_DATA}/" /usr/local/etc/rc.d/lidarr
-iocage exec ${JAIL_NAME} sysrc "lidarr_enable=YES"
+
+#
+# Change to user media
+iocage exec ${JAIL_NAME} chown -R media:media /config
+iocage exec ${JAIL_NAME} sysrc lidarr_enable="YES"
+iocage exec ${JAIL_NAME} sysrc lidarr_user="media"
+iocage exec ${JAIL_NAME} sysrc lidarr_group="media"
+iocage exec ${JAIL_NAME} sysrc lidarr_data_dir="/config"
 iocage exec ${JAIL_NAME} service lidarr start
-
-echo "lidarr installed"
-
-#
-# Make pkg upgrade get the latest repo
-iocage exec ${JAIL_NAME} mkdir -p /usr/local/etc/pkg/repos/
-iocage exec ${JAIL_NAME} cp -f /mnt/configs/FreeBSD.conf /usr/local/etc/pkg/repos/FreeBSD.conf
+echo "Lidarr installed"
 
 #
-# Upgrade to the lastest repo
-iocage exec ${JAIL_NAME} pkg upgrade -y
-iocage restart ${JAIL_NAME}
-
-#
-# remove /mnt/configs as no longer needed
-iocage fstab -r ${JAIL_NAME} ${CONFIGS_PATH} /mnt/configs nullfs rw 0 0
-
 # Make media owner of data directories
 chown -R media:media ${POOL_PATH}/${MEDIA_LOCATION}
 chown -R media:media ${POOL_PATH}/${TORRENTS_LOCATION}
 
 echo
-
-echo "lidarr should be available at http://${JAIL_IP}:8686"
+echo "Lidarr should be available at http://${JAIL_IP}:8686"
+echo "Movies will be located at "${POOL_PATH}/${MEDIA_LOCATION}/music
